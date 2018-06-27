24c454a69fbdd34f70352e34493fc504394b8dce
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2018-06-27 17:04:28 +0100

    Fix isPointerAligned() for the purecap ABI

--- Source/WTF/wtf/StdLibExtras.h
+++ Source/WTF/wtf/StdLibExtras.h
@@ -118,7 +118,11 @@ static const size_t MB = 1024 * 1024;
 
 inline bool isPointerAligned(void* p)
 {
+#ifdef __CHERI_PURE_CAPABILITY__
+    return __builtin_is_aligned(p, _MIPS_SZCAP/8);
+#else
     return !((vaddr_t)(p) & (sizeof(char*) - 1));
+#endif
 }
 
 inline bool is8ByteAligned(void* p)
