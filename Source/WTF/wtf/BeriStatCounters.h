95b8a330fbb403b80f42da6a4b1c8b7014ae9ea2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-01-31 14:49:57 +0000

    Only print statcounters output if VERBOSE env var is set

--- Source/WTF/wtf/BeriStatCounters.h
+++ Source/WTF/wtf/BeriStatCounters.h
@@ -4,8 +4,15 @@
 #include <statcounters.h>
 #include <string>
 
+static inline bool statcountersShouldPrint()
+{
+    static const char* verbose = getenv("VERBOSE");
+    return verbose && *verbose != '\0';
+}
+
 #define statcountersDeclareBank(bank_name) statcounters_bank_t bank_name
 static inline void statcountersStartPhase(statcounters_bank_t *stat_start, const std::string& phase) {
+    if (statcountersShouldPrint())
     fprintf(stderr, " -- Starting phase %s\n", phase.c_str());
     memset(stat_start, 0, sizeof(*stat_start));
     statcounters_sample(stat_start); // reset start
@@ -17,7 +24,9 @@ static inline void statcountersEndPhase(const statcounters_bank_t *stat_start, c
     statcounters_diff(&stat_diff, &stat_end, stat_start);
     // TODO: actually dump as CSV
     // statcounters_dump_with_phase(&stat_diff, phase.c_str());
-    fprintf(stderr, " -- Finished phase %s after %jd instructions\n", phase.c_str(), (intmax_t)stat_diff.inst);
+    if (statcountersShouldPrint())
+        fprintf(stderr, " -- Finished phase %s after %jd instructions(%jd user/%jd kernel)\n",
+           phase.c_str(), (intmax_t)stat_diff.inst, (intmax_t)stat_diff.mipsmem[2], (intmax_t)stat_diff.mipsmem[4]);
 }
 
 class BeriStatCountersScope {
